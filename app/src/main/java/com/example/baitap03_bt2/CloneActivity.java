fd759b28f667c50b35f9a1c778b9421aca3e85b8
LacDuong <lacduongldg212@gmail.com>
2025-02-21 10:30:20 +0700

    Commit BaiTap03_BT2

---
+++ app/src/main/java/com/example/baitap03_bt2/CloneActivity.java
@@ -0,0 +1,26 @@
+package com.example.baitap03_bt2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CloneActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_clone);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clone), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
